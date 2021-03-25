057923f1c290542d0919bb8164a651f2f409a866
Ryan <rjhuber83@gmail.com>
2021-03-25 11:21:28 -0400

    add google oauth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+// tells passport to use GoogleStrategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+  (req, res) => {}
+);
 
 // create dynamic ports
 const PORT = process.env.PORT || 5000;
